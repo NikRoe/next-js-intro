c8ec86d2aeeee3f7b617c920327fdc2e568bcfda
Niklas Röttger <niklas.roettger@outlook.de>
2022-09-06 14:39:17 +0200

    add next image and first image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Fische Shop</h1>;
+  return (
+    <>
+      <h1>Fische Shop</h1>
+      <Image
+        alt=""
+        src="https://unsplash.com/photos/9y7y26C-l4Y/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5MjcwNjYw&force=true&w=2400"
+        layout="responsive"
+        width={2400}
+        height={1599}
+      />
+    </>
+  );
 }
