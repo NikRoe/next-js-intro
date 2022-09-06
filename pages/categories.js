8522df341dc618d6c008fa9f2a37b00ddb2f2bdc
Niklas Röttger <niklas.roettger@outlook.de>
2022-09-06 12:21:53 +0200

    add first components and layout

---
+++ pages/categories.js
@@ -0,0 +1,8 @@
+export default function Categories() {
+  return (
+    <>
+      <h1>Kategorien</h1>
+      <p>Auflistung unserer Kategorien</p>
+    </>
+  );
+}
---
+++ pages/products.js
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <>
+      <h1>Produkte</h1>
+      <p>Auflistung unserer Produkte</p>
+    </>
+  );
+}
